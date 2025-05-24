e29f8dcd8a6238c7de71b29812d505b12307c4b4
AngelManuelGS <jaba.manuel@gmail.com>
2025-05-23 18:23:08 -0600

    Segunda actualizacion de codigo para mejorar la visibilidad en dispsitivos moviles

--- script.js
+++ script.js
@@ -223,9 +223,13 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
 const toggle = document.getElementById('menu-toggle');
 const navLinks = document.querySelector('.nav-links');
 
 toggle.addEventListener('click', () => {
   navLinks.classList.toggle('active');
+});
+document.getElementById('menu-toggle').addEventListener('click', function () {
+  document.getElementById('nav-links').classList.toggle('active');
 });
